024a292e26423023261baf5ce16bebc9a718de92
Daniel Ellis <daniel.ellis@ext.esa.int>
2025-11-14 23:00:10 +0000

    development

--- .github/prepublish/02-copy_context_with_extension.sh
+++ .github/prepublish/02-copy_context_with_extension.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 find . -type f -name "_context" -print0 | while IFS= read -r -d '' file; do
-      dir=$(dirname "$file")
-      cp "$file" "$dir/_context.json"
+      cp "$file" "$file.json"
 done
 
-
+echo "Context files copied with .json extension."
 
