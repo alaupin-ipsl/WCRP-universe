699618087f7f0bbea3fb8c1d3cf54f2b8446902c
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2025-11-21 21:12:53 +1100

    Add greenland

--- scripts/generate-region.py
+++ scripts/generate-region.py
@@ -9,6 +9,7 @@ def main():
     for id, drs_name, description, cf_standard_region, iso_region in (
         ("global", "glb", "The entire globe i.e. earth.", "global", None),
         ("antarctica", "ata", "Antarcica", "antarctica", "ata"),
+        ("greenland", "grl", "Greenland", "greenland", "grl"),
         (
             "30s-90s",
             "30S-90S",
